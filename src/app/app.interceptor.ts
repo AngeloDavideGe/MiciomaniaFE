f808ea8b1c189f9d0f2e159aae3d8cf46847da66
AngeloDavideGe <adgenovese2@gmail.com>
2025-07-11 13:20:43 +0200

    passaggio ad angular 18

--- src/app/app.interceptor.ts
+++ src/app/app.interceptor.ts
@@ -1,10 +1,4 @@
-import {
-  HTTP_INTERCEPTORS,
-  HttpEvent,
-  HttpHandler,
-  HttpInterceptor,
-  HttpRequest,
-} from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, finalize, Observable } from 'rxjs';
 import { environment } from '../environments/environment';
--- src/app/pages/manga/imports/manga.imports.ts
+++ src/app/pages/manga/imports/manga.imports.ts
@@ -1,5 +1,4 @@
 import { NgIf, NgFor, AsyncPipe } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { ErrorHttpComponent } from '../../../shared/components/errorhttp.component';
@@ -9,7 +8,6 @@ import { TabsMangaComponent } from '../shared/tabs-manga.component';
 export const manga_imports = [
   NgIf,
   NgFor,
-  HttpClientModule,
   RouterOutlet,
   AsyncPipe,
   FormsModule,
