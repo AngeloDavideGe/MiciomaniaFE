39d02e3a271ada4c03a7e1e4335c6d997d5b32f6
AngeloDavideGe <adgenovese2@gmail.com>
2025-09-23 17:33:48 +0200

    migliorie grafiche chat input

--- src/app/core/components/chat/components/chat-list/components/chat-group/components/chat-input/chat-input.component.ts
+++ src/app/core/components/chat/components/chat-list/components/chat-group/components/chat-input/chat-input.component.ts
@@ -9,59 +9,7 @@ import {
   selector: 'app-chat-input',
   standalone: true,
   imports: [FormsModule],
-  template: `
-    <div id="ChatInput">
-      <!-- Messaggio di risposta -->
-      @if(risposta) {
-      <div class="reply-container">
-        <div class="reply-indicator"></div>
-        <div class="reply-content">
-          <div class="reply-header">
-            <span class="reply-name">{{ risposta.idUser }}</span>
-            <span class="reply-label"></span>
-            <button class="reply-close" (click)="closeRisposta.emit()">
-              ✕
-            </button>
-          </div>
-          <p class="reply-message">{{ risposta.content }}</p>
-        </div>
-      </div>
-      } @if(modifica) {
-      <div class="reply-container">
-        <div class="reply-indicator"></div>
-        <div class="reply-content">
-          <div class="reply-header">
-            <span class="reply-label"></span>
-            <button class="reply-close" (click)="closeModifica.emit()">
-              ✕
-            </button>
-          </div>
-          <p class="reply-message">{{ modifica.content }}</p>
-        </div>
-      </div>
-      }
-
-      <!-- Input per il testo -->
-      <div class="input-container">
-        <input
-          [(ngModel)]="newMessage"
-          [placeholder]="
-            idUtente ? 'Scrivi un messaggio' : 'Effettua login per scrivere'
-          "
-          [disabled]="!idUtente"
-          (keyup.enter)="sendMessaggioFunc()"
-        />
-        <button (click)="sendMessaggioFunc()">
-          <svg viewBox="0 0 24 24" width="24" height="24" class="send-icon">
-            <path
-              fill="currentColor"
-              d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"
-            ></path>
-          </svg>
-        </button>
-      </div>
-    </div>
-  `,
+  templateUrl: './chat-input.component.html',
   styleUrl: './chat-input.component.scss',
 })
 export class ChatInputComponent {
@@ -74,19 +22,20 @@ export class ChatInputComponent {
   @Output() closeModifica = new EventEmitter<void>();
 
   sendMessaggioFunc(): void {
-    if (!!this.newMessage.trim()) {
-      if (this.modifica) {
-        this.sendMessaggio.emit({
-          idMessaggio: this.modifica.idMessaggio,
-          content: this.newMessage.trim(),
-        });
-      } else {
-        this.sendMessaggio.emit({
-          idMessaggio: null,
-          content: this.newMessage.trim(),
-        });
-      }
-      this.newMessage = '';
+    if (!this.newMessage.trim()) return;
+
+    if (this.modifica) {
+      this.sendMessaggio.emit({
+        idMessaggio: this.modifica.idMessaggio,
+        content: this.newMessage.trim(),
+      });
+    } else {
+      this.sendMessaggio.emit({
+        idMessaggio: null,
+        content: this.newMessage.trim(),
+      });
     }
+
+    this.newMessage = '';
   }
 }
